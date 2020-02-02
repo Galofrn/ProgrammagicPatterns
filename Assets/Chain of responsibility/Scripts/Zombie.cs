cf31e612504f97cbab1327954f5104a2711b84f3
Galofrn <galo.frn@gmail.com>
2020-02-02 15:03:05 -0300

    chain-of-responsibility-wip

---
+++ Assets/Chain of responsibility/Scripts/Zombie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Zombie enemy that walks towards a destination. Used in the chain of responsibility example.
+/// </summary>
+public class Zombie : Enemy
+{
+    [SerializeField] Animator _animator;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Move();
+    }
+
+    void Update()
+    {
+        _animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+    }
+}
---
+++ Assets/General/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Base class for every character who's a threat to the player.
+/// Takes damage and dies if it runs out of health.
+/// </summary>
+public class Enemy : Character
+{
+    [SerializeField] protected NavMeshAgent _navMeshAgent;
+    [SerializeField] protected Transform _destination;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        if (!_navMeshAgent)
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.speed = speed;
+        _navMeshAgent.angularSpeed = rotationSpeed;
+    }
+
+    protected override void Move()
+    {
+        if(_destination)
+            _navMeshAgent.SetDestination(_destination.position);
+    }
+
+    public virtual void Damage(int damageAmmount)
+    {
+        life -= damageAmmount;
+        if (life <= 0)
+            Kill();
+    }
+
+    protected virtual void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
